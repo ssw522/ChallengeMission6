ae6c24f4765d3adf7538ff3e7daddc8a0d005db2
SangWoo Shin <ssw522b@naver.com>
2020-04-27 23:11:38 +0900

    Initial Commit

---
+++ ChallengeMission6/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChallengeMission6
+//
+//  Created by 신상우 on 2020/04/27.
+//  Copyright © 2020 SangWoo Shin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
